57c5e938954455ba75b2f2567f0c04d7794b3262
Luisangel Parra <lfaria@uninorte.edu.co>
2025-03-17 15:44:19 -0500

    Feat: Added Dog class

---
+++ src/core/Dog.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package core;
+
+/**
+ *
+ * @author asecheverria
+ */
+public class Dog {
+
+    private String name;
+    private int age;
+
+    public Dog(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+}
